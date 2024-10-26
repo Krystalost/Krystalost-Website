88ec1e9a398458b52fc0bbb33ac600609f3f3ee5
Krystalost <abdullahkayrakaraman@gmail.com>
2024-10-26 08:49:31 +0300

    update

--- steam.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-// Buton öğesini seç
-const button = document.getElementById('interactiveButton');
-
-// Butona tıklandığında mesaj göster
-button.addEventListener('click', function() {
-    alert('Krystalost Steam Profiline Yonlendiriliyorsunuz!');
-});
